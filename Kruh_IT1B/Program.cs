b449a6989eff8d5b0bf8ec1656f9e9ed9d844f24
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-02 10:53:09 +0100

    funguje

--- Kruh_IT1B/Program.cs
+++ Kruh_IT1B/Program.cs
@@ -6,7 +6,72 @@ namespace Kruh_IT1B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int menuSelection;
+            double radius;
+            double diameter;
+            while (true)
+            {
+                Console.Clear();
+                menuSelection = Menu();
+                if (menuSelection == 1)
+                {
+                    radius = UserInputRadius();
+                    Console.WriteLine(GetCircumference(radius, true));
+                    Console.WriteLine(GetContent(radius, true));
+                }
+                else
+                {
+                    diameter = UserInputDiameter();
+                    Console.WriteLine(GetCircumference(diameter, false));
+                    Console.WriteLine(GetContent(diameter, false));
+                }
+                Console.ReadLine();
+            }            
+        }
+
+        static int Menu()
+        {
+            Console.WriteLine(" CIRCLE");
+            Console.WriteLine("---------------");
+            Console.WriteLine(" 1 - enter radius");
+            Console.WriteLine(" 2 - enter diameter");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static double UserInputRadius()
+        {
+            Console.Write(" Enter radius value: ");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double UserInputDiameter()
+        {
+            Console.Write(" Enter diameter value: ");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static string GetCircumference(double value, bool isRadius)
+        {
+            if (isRadius)
+            {
+                return $"o = {Math.Round(2 * Math.PI * value, 2)}";
+            }
+            else
+            {
+                return $"o = {Math.Round(Math.PI * value, 2)}";
+            }
+        }
+
+        static string GetContent(double value, bool isRadius)
+        {
+            if (isRadius)
+            {
+                return $"S = {Math.Round(Math.PI * value * value, 2)}";
+            }
+            else
+            {
+                return $"S = {Math.Round(Math.PI * ((value * value) / 4), 2)}";
+            }
         }
     }
 }
